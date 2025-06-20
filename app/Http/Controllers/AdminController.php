bbe879c4897aee0e27c635d54ef8fdd4dabf62ac
Muhammad15246 <faizybangash05@gmail.com>
2025-06-20 12:36:29 +0500

    admin-login-done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin; 
 
 class AdminController extends Controller
 {
@@ -12,13 +13,23 @@ class AdminController extends Controller
             'password' => 'required|string',
         ]);
         if($validate){
+            $admin = Admin::where('name', $req->input('name'))
+                            ->where('password', $req->input('password'))
+                            ->first();
+            // // return $req->input('name') . " logged in successfully";
+            // $admin = Admin::where([
+            //     'name' => $req->name,
+            //     'password' => $req->password,
+            // ])->first();
+            if($admin){
             
-            // return $req->input('name') . " logged in successfully";
-            return $req->input();
+            return view('Auth.AdminDashboard', [
+                'admin' => $admin,
+            ]);
         }else{
             return "Invalid credentials";
         }
-        
+    }
         
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,3 @@ use App\Http\Controllers\AdminController;
 
 Route::view('/admin-login', 'Auth.AdminLogin');
 Route::post('/admin-login', [AdminController::class, 'login']);
-// Route::post('/admin-dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
